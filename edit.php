3c76c1581131835ebeae0de6e83c1b5a8e2d9c2b
Kaung Thant <kaungthant9513@gmail.com>
2022-07-27 12:45:57 +0630

    create edit.php

--- edit.php
+++ edit.php
@@ -28,7 +28,7 @@ if($result) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Page</title>
+    <title>Edit Page</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="fontawesome/css/all.min.css">
 </head>
